9101367ba3d697caf7979ae3c43d83e41f3b232d
VivekOwhal <vivek123.co@gmail.com>
2023-01-03 16:28:01 +0530

    pasword validation added to main program

--- TransactionMachine.java
+++ TransactionMachine.java
@@ -2,5 +2,25 @@ import java.util.Scanner;
 
 class TransactionMachine
 {
+    Scanner sc = new Scanner(System.in);
+    void enterPassword()
+    {
+        int password=1234;
+        System.out.println("============= Welcome to Transaction Macine =============");
+        System.out.println(" Please Enter Your Password ");
+        password = sc.nextInt();
+        if(password == 1234)
+        {
+            System.out.println("Success !!!");
+        }
+        else
+        {
+            System.out.println("Incorrect Password !!! \nTransaction Canceled....");
+        }
+    }
     
+    public static void main(String[] args)
+    {
+        
+    }
 }
